cd53917f9e2b627ccfb3beadc58608e4ba7a05ac
kingj2 <james.king@emc.com>
2015-10-05 14:40:21 -0400

    Added System Name, System Description, and System Capabilities Support

--- decode/lldp/systemCaps.js
+++ decode/lldp/systemCaps.js
@@ -2,7 +2,8 @@ function systemCaps() {
 }
 
 systemCaps.prototype.decode = function(tlv, raw_packet, offset) {
-  return;
+  tlv.sysCaps = raw_packet.readUInt16BE(offset, true);
+  tlv.enabledCaps = raw_packet.readUInt16BE(offset, true);
 }
 
 module.exports = systemCaps;
--- decode/lldp/systemDescription.js
+++ decode/lldp/systemDescription.js
@@ -1,8 +1,8 @@
 function systemDescription() {
 }
 
-systemDescription.prototype.decode = function(tlv, raw_packet, offset) {
-  return;
+systemDescription.prototype.decode = function(tlv, raw_packet, offset, length) {
+  tlv.systemDescription = raw_packet.toString('utf8',offset,offset+length);
 }
 
 module.exports = systemDescription;

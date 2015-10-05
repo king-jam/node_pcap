596ce7e2b435d45af3c7f2e026131bcac9e4e55a
hanasu <bodowd@worcester.edu>
2015-10-05 13:49:28 -0400

    Fixes to TTL

--- decode/lldp/ttl.js
+++ decode/lldp/ttl.js
@@ -1,9 +1,8 @@
 function ttl() {
-	this.ttl = undefined;	
 }
 
-chassisId.prototype.decode = function(tlv, raw_packet, offset) {
-	this.ttl = raw_packet.readUInt16BE(offset+1, true);
+ttl.prototype.decode = function(tlv, raw_packet, offset) {
+	tlv.ttlSeconds = raw_packet.readUInt16BE(offset, true);
 }
 
 module.exports = ttl;
